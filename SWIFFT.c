67555734fd5275bc26c4e71d9366e1e8844dfc34
PSCucalgary <pscucalgary@gmail.com>
2016-11-23 17:59:00 -0700

    More

--- SWIFFT.c
+++ SWIFFT.c
@@ -5,11 +5,41 @@ const int N = 2;
 const int M = 16;
 const int P = 257;
 
-void karatsuba(int * res, int f[N], int g[N]) {
-    
+struct Poly {
+	int degree;
+	int sizeCoefs;
+	int * coefs;
+};
+
+struct Poly add(struct Poly a, struct Poly b) {
+	
+	//FIX
+	struct Poly res;
+	//res.degree = a.degree;
+	res.coefs = malloc(res.degree * sizeof(int));
+	
+	for (int i = 0; i < a.sizeCoefs; i++) {
+		*(res.coefs + (i * sizeof(int))) = *(a.coefs + (i * sizeof(int)))
+											+ *(b.coefs + (i * sizeof(int)));
+	}
 }
 
-void classical(int * res, int f[N], int g[N]) {
+struct Poly dsub(struct Poly a, struct Poly d1, struct Poly d2) {
+	struct Poly res;
+	res.degree = a.degree;
+}
+
+struct Poly karatsuba(struct Poly f, struct Poly g) {
+	if (n == 2) {
+		
+	}
+	
+	struct Poly a = karatsuba(uf, ug);
+	struct Poly d = karatsuba(lf, lg);
+	struct Poly e = dsub(karatsuba(add(uf, lf), add(ug, lg)), a, d);
+}
+
+/*void classical(int * res, int f[N], int g[N]) {
 	for (int i = 0; i < N; i++) {
 		int coef = 0;
 		for (int j = 0; j <= i / 2; j++) {
@@ -19,16 +49,17 @@ void classical(int * res, int f[N], int g[N]) {
 		}
 		*(res + (i * sizeof(int))) = coef;
 	}
-}
+}*/
 
 int main(int argc, char * argv[]) {
-	int f[N];
-	int g[N];
-	f[0] = 1; f[1] = 2;
-	g[0] = 3; g[1] = 1;
-	int * r = malloc(N * sizeof(int));
+	struct Poly f;
+	struct Poly g;
 	
-	classical(r, f, g);
+	f.degree = N; g.degree = N; f.sizeCoefs = N; g.sizeCoefs = N;
+	
+	//classical(r, f, g);
+	
+	
 	
 	for (int i = 0; i < N; i++) {
 		printf("%d\n", *(r + (i * sizeof(int))));
