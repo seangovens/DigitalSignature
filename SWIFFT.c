c1ed377add68f5688a356e3b83bba0e8557aaa33
PSCucalgary <pscucalgary@gmail.com>
2016-11-29 21:49:45 -0700

    Test and correct KA function

--- SWIFFT.c
+++ SWIFFT.c
@@ -25,9 +25,11 @@ struct Poly add(struct Poly a, struct Poly b) {
     int aMinDeg = a.degree - a.sizeCoefs + 1, bMinDeg = b.degree - b.sizeCoefs + 1;
     int minDeg = min(aMinDeg, bMinDeg);
     res.sizeCoefs = res.degree - minDeg + 1;
-	res.coefs = malloc(res.sizeCoefs * sizeof(int));
+    printf("BEFORE %d\n", res.sizeCoefs);
+	res.coefs = (int *) malloc(res.sizeCoefs * sizeof(int));
+	printf("AFTER\n");
 	
-	printf("Degree: %d\nSizeCoefs: %d\n", res.degree, res.sizeCoefs);
+	//printf("Degree: %d\nSizeCoefs: %d\n", res.degree, res.sizeCoefs);
 	
     int currDeg = res.degree;
 	for (int i = 0; i < res.sizeCoefs; i++) {
@@ -39,7 +41,7 @@ struct Poly add(struct Poly a, struct Poly b) {
             *(res.coefs + (i * sizeof(int))) = *(a.coefs + ((a.degree - currDeg)*sizeof(int))) 
 												+ *(b.coefs + ((b.degree - currDeg)*sizeof(int)));
                                                 
-            printf("a val: %d at %d b val: %d at %d index: %d\n", *(a.coefs + ((a.degree - currDeg)*sizeof(int))), a.degree - currDeg, *(b.coefs + ((b.degree - currDeg)*sizeof(int))), b.degree - currDeg, i);
+            //printf("a val: %d at %d b val: %d at %d index: %d\n", *(a.coefs + ((a.degree - currDeg)*sizeof(int))), a.degree - currDeg, *(b.coefs + ((b.degree - currDeg)*sizeof(int))), b.degree - currDeg, i);
         }
         currDeg--;
 	}
@@ -67,7 +69,8 @@ struct Poly dsub(struct Poly a, struct Poly d1, struct Poly d2) {
 	struct Poly res1 = add(d1Neg, d2Neg);
 	struct Poly res2 = add(res1, a);
 	
-	free(res1.coefs); free(d1Neg.coefs); free(d2Neg.coefs);
+	//printf("%d %d %d\n", res1.coefs, d1Neg.coefs, d2Neg.coefs);
+	free(res1.coefs); free(d1Neg.coefs); //free(d2Neg.coefs);
 	
 	return res2;
 }
@@ -75,28 +78,36 @@ struct Poly dsub(struct Poly a, struct Poly d1, struct Poly d2) {
 struct Poly karatsuba(struct Poly f, struct Poly g) {
 	struct Poly res;
 	
-	if (f.sizeCoefs == 1) {
+	printf("CURR COEFS: %d\n", f.sizeCoefs);
+	
+	if (f.sizeCoefs <= 1) {
+		printf("End recursion\n");
 		res.degree = f.degree + g.degree;
 		res.sizeCoefs = 1;
+		res.coefs = malloc(sizeof(int));
 		*res.coefs = *f.coefs * *g.coefs;
 	}
 	else {
 		int halfCoefs = f.sizeCoefs / 2;
 		int halfDeg = f.degree / 2;
 		struct Poly uf; uf.degree = halfDeg; uf.sizeCoefs = halfCoefs; uf.coefs = f.coefs;
-		struct Poly lf; lf.degree = halfDeg; uf.sizeCoefs = halfCoefs; lf.coefs = f.coefs + (halfCoefs * sizeof(int));
+		struct Poly lf; lf.degree = halfDeg; lf.sizeCoefs = halfCoefs; lf.coefs = f.coefs + (halfCoefs * sizeof(int));
+		//printf("halfCoefs f: %d\n", halfCoefs);
+		//printf("f address: %d, lf address: %d\n", f.coefs, lf.coefs);
 		
 		halfCoefs = g.sizeCoefs / 2;
 		halfDeg = g.degree / 2;
 		struct Poly ug; ug.degree = halfDeg; ug.sizeCoefs = halfCoefs; ug.coefs = g.coefs;
-		struct Poly lg; lg.degree = halfDeg; ug.sizeCoefs = halfCoefs; lg.coefs = g.coefs + (halfCoefs * sizeof(int));
+		struct Poly lg; lg.degree = halfDeg; lg.sizeCoefs = halfCoefs; lg.coefs = g.coefs + (halfCoefs * sizeof(int));
+		//printf("halfCoefs g: %d\n", halfCoefs);
+		//printf("g address: %d, lg address: %d\n", g.coefs, lg.coefs);
         
-        int n = max(f.degree, g.degree);
+        int n = max(f.degree, g.degree) + 1;
 		
 		struct Poly up = karatsuba(uf, ug);
-        up.degree += n;
 		struct Poly low = karatsuba(lf, lg);
 		struct Poly mid = dsub(karatsuba(add(uf, lf), add(ug, lg)), up, low);
+		up.degree += n;
         mid.degree += n / 2;
 		
 		struct Poly resT = add(up, mid);
@@ -132,25 +143,27 @@ int main(int argc, char * argv[]) {
 	struct Poly g;
 	struct Poly r;
 	
-	f.degree = 2; g.degree = 1; f.sizeCoefs = 3; g.sizeCoefs = 2;
-	printf("%d\n", f.sizeCoefs * sizeof(int));
+	f.degree = 3; g.degree = 3; f.sizeCoefs = 4; g.sizeCoefs = 4;
+	//printf("%d\n", f.sizeCoefs * sizeof(int));
 	f.coefs = (int *) malloc((f.sizeCoefs+sizeof(int)) * sizeof(int));
 	g.coefs = (int *) malloc((g.sizeCoefs/*+sizeof(int)*/) * sizeof(int));
 	int * test = malloc(sizeof(int));
 	
-	printf("Addresses f: %d %d %d\nAddresses g: %d %d\n", f.coefs + (0 * sizeof(int)), f.coefs + (1 * sizeof(int)), f.coefs + (2 * sizeof(int)), g.coefs + (0 * sizeof(int)), g.coefs + (1 * sizeof(int)));
-	printf("Test address: %d\n", test);
+	//printf("Addresses f: %d %d %d\nAddresses g: %d %d\n", f.coefs + (0 * sizeof(int)), f.coefs + (1 * sizeof(int)), f.coefs + (2 * sizeof(int)), g.coefs + (0 * sizeof(int)), g.coefs + (1 * sizeof(int)));
+	//printf("Test address: %d\n", test);
 	
-	*f.coefs = 1; *(f.coefs + sizeof(int)) = 4; *(f.coefs + (2 * sizeof(int))) = 1;
-	printCoefs(f);
-	*g.coefs = 2; *(g.coefs + sizeof(int)) = 1;
-	printCoefs(f);
-	printCoefs(g);
+	*f.coefs = 1; *(f.coefs + sizeof(int)) = 4; *(f.coefs + (2 * sizeof(int))) = 1; *(f.coefs + (3 * sizeof(int))) = 4;
+	//printCoefs(f);
+	*g.coefs = 2; *(g.coefs + sizeof(int)) = 1; *(g.coefs + (2 * sizeof(int))) = 3; *(g.coefs + (3 * sizeof(int))) = 2;
+	//printCoefs(f);
+	//printCoefs(g);
 	//classical(r, f, g);
 	
-	printf("Size: %d\n", sizeof(int));
+	printf("Orig f address: %d\n", f.coefs);
+	printf("Orig g address: %d\n", g.coefs);
 	
-	r = add(f, g);
+	r = karatsuba(f, g);
+	printf("END\n");
 	
 	for (int i = 0; i < r.sizeCoefs; i++) {
 		printf("%d\n", *(r.coefs + (i * sizeof(int))));
